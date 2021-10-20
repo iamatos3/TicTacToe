1048e3300a8ba59c6673ce97ec3ed25fdb5d9572
iamatos3 <matosian93@gmail.com>
2021-10-19 22:34:17 -0400

    Created a game function and updated some html created a game function to determine a winner or draw and not allow the user to select a box that has already been selected.

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 const authEvents = require('./auth/events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const gameEvents = require('./auth/events')
 
 $(() => {
   // your JS code goes here
@@ -15,19 +16,36 @@ $(() => {
 
   // Game Grid Events
   let currentPlayer = 'X'
+  const gameBoard = ["", "", "", "", "", "", "", "", ""]
+  let endGame = false
+
 
   const onBoxClick = (event) => {
+    console.log(endGame)
+   if (!endGame) {
     console.log('click')
 
     const box = $(event.target)
 
     box.css('background', 'transparent')
     box.text(currentPlayer)
+    gameBoard[event.target.id] = currentPlayer
+    endGame = gameEvents.checkWin(currentPlayer, gameBoard, endGame)
 
     currentPlayer = currentPlayer === 'O' ? 'X' : 'O'
-  }
+    // winnerCheck()
+    // $(this).addClass(currentPlayer)
+    console.log(gameBoard)
+  }}
   $('.box').on('click', onBoxClick)
 
+  // const winnerCheck = () => {
+  //   const box = $(event.target)
+  //   if (box.text === 'X') {
+  //     console.log('hit')
+  //   }
+  // }
+
   // Game Events
-  $('#new-game').on('click', events.onNewGame)
+  $('#new-game').on('click', gameEvents.onNewGame)
 })
--- app/auth/events.js
+++ app/auth/events.js
@@ -4,6 +4,8 @@ const api = require('./api')
 
 const ui = require('./ui')
 
+
+// Authorization Functions
 const onSignUp = function (event) {
   event.preventDefault()
 
@@ -44,16 +46,118 @@ const onSignOut = function () {
     .catch(ui.signOutFailure)
 }
 
+// Game Button Function
 const onNewGame = () => {
   api.newGame()
     .then(ui.onNewGameSuccess)
     .catch(ui.newGameFailure)
 }
 
+// Game Function
+const checkWin = function (currentPlayer, gameBoard) {
+  if (gameBoard[0] === currentPlayer &&
+      gameBoard[1] === currentPlayer &&
+      gameBoard[2] === currentPlayer
+  ) {
+    console.log(currentPlayer, 'Wins!')
+    endGame = true
+    return endGame
+  } else if (
+    gameBoard[0] === currentPlayer &&
+    gameBoard[3] === currentPlayer &&
+    gameBoard[6] === currentPlayer
+  ) {
+    console.log(currentPlayer, 'Wins!')
+    endGame = true
+    return endGame
+  } else if (
+    gameBoard[0] === currentPlayer &&
+    gameBoard[4] === currentPlayer &&
+    gameBoard[8] === currentPlayer
+  ) {
+    console.log(currentPlayer, "Wins!")
+    endGame = true
+    return endGame
+  } else if (
+    gameBoard[1] === currentPlayer &&
+    gameBoard[4] === currentPlayer &&
+    gameBoard[7] === currentPlayer
+  ) {
+    console.log(currentPlayer, "Wins!")
+    endGame = true
+    return endGame
+  } else if (
+    gameBoard[2] === currentPlayer &&
+    gameBoard[5] === currentPlayer &&
+    gameBoard[8] === currentPlayer
+  ) {
+    console.log(currentPlayer, "Wins!")
+    endGame = true
+    return endGame
+  } else if (
+    gameBoard[2] === currentPlayer &&
+    gameBoard[4] === currentPlayer &&
+    gameBoard[6] === currentPlayer
+  ) {
+    console.log(currentPlayer, "Wins!")
+    endGame = true
+  } else if (
+    gameBoard[3] === currentPlayer &&
+    gameBoard[4] === currentPlayer &&
+    gameBoard[5] === currentPlayer
+  ) {
+    console.log(currentPlayer, "Wins!")
+    endGame = true
+    return endGame
+  } else if (
+    gameBoard[6] === currentPlayer &&
+    gameBoard[7] === currentPlayer &&
+    gameBoard[8] === currentPlayer
+  ) {
+    console.log(currentPlayer, "Wins!")
+    endGame = true
+    return endGame
+  } else if (
+    !gameBoard.includes("")
+  ) {
+    console.log('The game is a Draw. Play again!')
+    endGame = true
+    return endGame;
+  } else {
+    console.log('Game is still in play. Keep going!')
+    endGame = false
+    return endGame
+   }
+  }
+
+// const winningCombos = [
+//   [0, 1, 2],
+//   [0, 3, 6],
+//   [0, 4, 8],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [2, 4, 6],
+//   [3, 4, 5],
+//   [6, 7, 8]
+// ]
+
+// const checkPlayer = (n) => {
+//   return winningCombos.some(win => {
+//     winningCombos.every(cell => gameBoard[cell] === n)
+//   })
+// }
+
+// const winnerCheck = (a, b) => {
+//   return checkPlayer(a) || checkPlayer(b)
+// }
+
+// winnerCheck('X', 'O')
+
 module.exports = {
   onSignUp,
   onSignIn,
   onChangePassword,
   onSignOut,
-  onNewGame
+  onNewGame,
+  checkWin
 }
